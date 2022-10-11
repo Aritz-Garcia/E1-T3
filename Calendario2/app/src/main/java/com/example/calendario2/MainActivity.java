63e0292d0531f1c4bf9a9dcb40916677e660017f
Aritz-Garcia <aritz.garciaba@elorrieta-errekamari.com>
2022-10-11 10:50:58 +0200

    salas

--- Calendario2/app/src/main/java/com/example/calendario2/MainActivity.java
+++ Calendario2/app/src/main/java/com/example/calendario2/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.calendario2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.Menu;
 
 import com.google.android.material.navigation.NavigationView;
@@ -37,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_agenda, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_agenda, R.id.nav_salas, R.id.nav_slideshow)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
--- Calendario2/app/src/main/java/com/example/calendario2/ui/salas/SalasViewModel.java
+++ Calendario2/app/src/main/java/com/example/calendario2/ui/salas/SalasViewModel.java
@@ -1,14 +1,14 @@
-package com.example.calendario2.ui.gallery;
+package com.example.calendario2.ui.salas;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class GalleryViewModel extends ViewModel {
+public class SalasViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public GalleryViewModel() {
+    public SalasViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is gallery fragment");
     }
