d8f9bd19d810eb28570ff56283b9ca9eec535e8f
Aritz-Garcia <aritz.garciaba@elorrieta-errekamari.com>
2022-10-18 14:05:18 +0200

    linearlayout_kontaktua

--- OnPlan/app/src/main/java/com/e1t3/onplan/ui/kontaktua/KontaktuaFragment.java
+++ OnPlan/app/src/main/java/com/e1t3/onplan/ui/kontaktua/KontaktuaFragment.java
@@ -6,7 +6,6 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import com.e1t3.onplan.databinding.FragmentKontaktuaBinding;
 
